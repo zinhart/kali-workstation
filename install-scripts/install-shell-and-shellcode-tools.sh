9836462f0793e861189c2a2332e034f2bac7291b
zinhart <aromzinhartdegraca@gmail.com>
2022-04-07 11:04:35 -0700

    Install dependencies for shellter to work. Install ysoserial to /opt.

--- install-scripts/install-shell-and-shellcode-tools.sh
+++ install-scripts/install-shell-and-shellcode-tools.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 echo 'INSTALLING SHELL AND SHELLCODE TOOOLS'
 sudo apt install -y evil-winrm powercat rlwrap shellter
+# 32 bit dependencies for shellter to run(must be run as root)
+dpkg --add-architecture i386 && apt-get update && apt-get install wine32
+#ysoserial rmi injections
+sudo git clone https://github.com/zinhart/ysoserial.git /opt/ysoserial
