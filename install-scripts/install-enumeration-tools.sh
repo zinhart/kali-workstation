93de1cfae5056c03adb6dcd1b9f12d8910e3ed48
zinhart <aromzinhartdegraca@gmail.com>
2022-04-07 11:05:13 -0700

    Organization and sudo permissions

--- install-scripts/install-enumeration-tools.sh
+++ install-scripts/install-enumeration-tools.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo 'INSTALLING ENUMERATION TOOLS'
 sudo apt install -y snmpenum gobuster
+# JAVA RMI enumation tools
+sudo git clone https://github.com/zinhart/remote-method-guesser.git /opt/remote-method-guesser
 # I've found the most reliable way to transfer enum scripts ftp.
 # So it makes to store these local enumeration scripts in the ftp user home directory.
 # Consequently this means that the ftp user home directory must exist, which means that this script cannot be run fore configure-pureftpd.sh
@@ -8,11 +10,10 @@ sudo apt install -y snmpenum gobuster
 mkdir -p /home/vagrant/ftphome
 echo 'INSTALLING LOCAL ENUMERATION TOOLS'
 # possibly will have this be a cron job in the future
-curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh -o /home/vagrant/ftphome/linpeas.sh
-curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany_ofs.exe -o /home/vagrant/ftphome/winpeas.exe
+sudo curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh -o /home/vagrant/ftphome/linpeas.sh
+sudo curl -L https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany_ofs.exe -o /home/vagrant/ftphome/winpeas.exe
 # My active directory scripts created during pwk.
 # Repo needs to be public or we need to pass a personal access token 
-git clone https://github.com/zinhart/domain-enumeration.git /home/vagrant/ftphome/domain-enumeration
-# JAVA RMI enumation tools
-git clone https://github.com/zinhart/remote-method-guesser.git /opt/remote-method-guesser
+sudo git clone https://github.com/zinhart/domain-enumeration.git /home/vagrant/ftphome/domain-enumeration
+
 
