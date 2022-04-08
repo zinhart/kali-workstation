3b98471592cc9e49c23a821d0912fa5d6ba0bff7
zinhart <aromzinhartdegraca@gmail.com>
2022-04-08 08:45:26 -0700

    Added tool for extracting sql server pw hashes

--- install-scripts/install-cracking-tools.sh
+++ install-scripts/install-cracking-tools.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo 'INSTALLING CRACKING TOOLS'
 sudo apt install -y crowbar fcrackzip rarcrack
+#sql server password hash cracking tools
+sudo git clone https://github.com/zinhart/Powershell-PostExploitation.git /opt/powershell-PostExploitation
+sudo cp -r /opt/powershell-postexploitation/Invoke-MDFHashes /home/vagrant/ftphome/invoke-mdfhashes
