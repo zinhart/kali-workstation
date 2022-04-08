320a4820dbb3e7abea8bbf3a8217fb05e70d8e73
zinhart <aromzinhartdegraca@gmail.com>
2022-04-08 08:52:09 -0700

    Typo fixes

--- install-scripts/install-cracking-tools.sh
+++ install-scripts/install-cracking-tools.sh
@@ -2,6 +2,6 @@
 echo 'INSTALLING CRACKING TOOLS'
 sudo apt install -y crowbar fcrackzip rarcrack
 #sql server password hash cracking tools
-sudo git clone https://github.com/zinhart/Powershell-PostExploitation.git /opt/powershell-PostExploitation
+sudo git clone https://github.com/zinhart/Powershell-PostExploitation.git /opt/powershell-post-exploitation
 sudo cp -r /opt/powershell-post-exploitation/Invoke-MDFHashes /home/vagrant/ftphome/invoke-mdfhashes
 sudo rm -rf /opt/powershell-post-exploitation
