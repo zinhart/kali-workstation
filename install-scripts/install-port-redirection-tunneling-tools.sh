efc1f2ed50226ede42755856c35ef6f5ba9bd4ea
zinhart <aromzinhartdegraca@gmail.com>
2022-04-18 14:09:24 -0700

    Big number here: Adding sshuttle. Massive improvement over ssh dynamic tunnels. Definently our second choice when a standard ssh tunnel is too slow.

--- install-scripts/install-port-redirection-tunneling-tools.sh
+++ install-scripts/install-port-redirection-tunneling-tools.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # port redirection/tunneling
 echo 'INSTALLING PORT REDIRECTION AND TUNNELING TOOLS'
-sudo apt install -y httptunnel rinetd chisel proxychains
-# plink has a dependency on med-config which just fucks the
-# unattended install, have not found a way around this.
+sudo apt install -y sshuttle httptunnel rinetd chisel proxychains
+# plink has a dependency on med-config which just fucks the unattended install, have not found a way around this.
