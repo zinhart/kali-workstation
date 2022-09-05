95e34f546e7619983ef47d0f640b8de398c11f94
zinhart <aromzinhartdegraca@gmail.com>
2022-09-05 09:24:50 -0700

    Adding submodule update to pull php shell.

--- install-scripts/install-shell-and-shellcode-tools.sh
+++ install-scripts/install-shell-and-shellcode-tools.sh
@@ -2,4 +2,5 @@
 echo 'INSTALLING SHELL AND SHELLCODE TOOLS'
 sudo apt install -y evil-winrm powercat rlwrap shellter
 sudo git clone https://github.com/zinhart/webshells /home/vagrant/ftphome/webshells
+cd /home/vagrant/ftphome/webshells && git submodule update  --init --recursive
 sudo git clone https://github.com/zinhart/windows-kernel-exploits.git /home/vagrant/ftphome/windows-kernel-exploits
