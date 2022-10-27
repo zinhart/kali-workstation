96a7fbd1fb847bb9fd0ab5caaafc0c791f0f7a03
zinhart <aromzinhartdegraca@gmail.com>
2022-10-27 13:51:23 -0700

    Adding csharp compilation tools  to cross compilation install script

--- install-scripts/install-cross-compilation-tools.sh
+++ install-scripts/install-cross-compilation-tools.sh
@@ -2,3 +2,5 @@
 # cross compilation tools
 echo 'INSTALLING CROSS COMPILTATION TOOLS'
 sudo dpkg --add-architecture i386 && sudo apt update && sudo apt -y install wine32 mingw-w64;
+# csharp
+sudo apt -y install mono-complete;
