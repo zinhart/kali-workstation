3d05501dd928091890bee3bc342c152a90ffdbaa
zinhart <aromzinhartdegraca@gmail.com>
2022-10-29 15:47:17 -0700

    Fixing sillyness causing modules not to display

--- install-scripts/configure-login-shell.sh
+++ install-scripts/configure-login-shell.sh
@@ -1,9 +1,5 @@
-cp /home/vagrant/.config/powershell/Microsoft.PowerShell_profile.ps1  /home/vagrant/.config/powershell/profile.ps1;
-echo '# Shows navigable menu of all options when hitting Tab' >> /home/vagrant/.config/powershell/profile.ps1;
-echo 'Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete' >> /home/vagrant/.config/powershell/profile.ps1;
-echo '# Autocompletion for arrow keys' >> /home/vagrant/.config/powershell/profile.ps1;
-echo 'Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward' >> /home/vagrant/.config/powershell/profile.ps1;
-echo 'Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward' >> /home/vagrant/.config/powershell/profile.ps1;
-# according do microsft profile.ps1 takes precedence over Microsoft.PowerShell_profile.ps1, I'm likely to forget that so appending .bak makes the change visual
-mv /home/vagrant/.config/powershell/Microsoft.PowerShell_profile.ps1 /home/vagrant/.config/powershell/Microsoft.PowerShell_profile.ps1.bak;
+touch /home/vagrant/.config/powershell/profile.ps1;
+echo 'Import-Module PSReadline' >> /home/vagrant/.config/powershell/profile.ps1;
+pwsh -command 'Import-Module PowerShellGet; Install-Module Get-ChildItemColor -Force;';
+echo 'Set-Alias l Get-ChildItem -option AllScope' >> /home/vagrant/.config/powershell/profile.ps1;
 sudo chsh -s vagrant /usr/bin/pwsh;
