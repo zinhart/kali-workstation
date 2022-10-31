e57d9e1c4f9f148011b1daf44c7438662c37c321
zinhart <aromzinhartdegraca@gmail.com>
2022-10-30 19:14:16 -0700

    Accidentally removed tab auto complate from pwsh

--- install-scripts/configure-login-shell.sh
+++ install-scripts/configure-login-shell.sh
@@ -1,5 +1,8 @@
 touch /home/vagrant/.config/powershell/profile.ps1;
 echo 'Import-Module PSReadline' >> /home/vagrant/.config/powershell/profile.ps1;
 pwsh -command 'Import-Module PowerShellGet; Install-Module Get-ChildItemColor -Force;';
+echo '# Shows navigable menu of all options when hitting Tab' >> /home/vagrant/.config/powershell/profile.ps1;
+echo 'Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete' >> /home/vagrant/.config/powershell/profile.ps1;
 echo 'Set-Alias l Get-ChildItem -option AllScope' >> /home/vagrant/.config/powershell/profile.ps1;
+# to do copy Sampleprofile in psreadline and of course study its capabilities
 sudo chsh -s vagrant /usr/bin/pwsh;
