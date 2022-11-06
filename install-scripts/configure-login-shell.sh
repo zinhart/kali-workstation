4818185272af87d482c1081ce75ec046625c94f1
zinhart <aromzinhartdegraca@gmail.com>
2022-11-06 11:24:01 -0800

    Adding to do for installing PoshRSJob

--- install-scripts/configure-login-shell.sh
+++ install-scripts/configure-login-shell.sh
@@ -5,4 +5,6 @@ echo '# Shows navigable menu of all options when hitting Tab' >> /home/vagrant/.
 echo 'Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete' >> /home/vagrant/.config/powershell/profile.ps1;
 echo 'Set-Alias l Get-ChildItem -option AllScope' >> /home/vagrant/.config/powershell/profile.ps1;
 # to do copy Sampleprofile in psreadline and of course study its capabilities
+# to do install Posh-SSH
+# to do install PoshRSJob
 sudo chsh -s vagrant /usr/bin/pwsh;
