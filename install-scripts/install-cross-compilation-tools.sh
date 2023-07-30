cd3f3d26fb9ce2918523274fd9fa0d8ff7566aa8
zinhart <aromzinhartdegraca@gmail.com>
2023-07-29 19:47:24 -0700

    Fixes issues in cross compilation script preventing silent install

--- install-scripts/install-cross-compilation-tools.sh
+++ install-scripts/install-cross-compilation-tools.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # cross compilation tools
+# echo 'INSTALLING CROSS COMPILTATION TOOLS'
+# sudo dpkg --add-architecture i386 && sudo apt update && sudo apt -y install wine32 mingw-w64;
+ # csharp
+# sudo apt -y install mono-complete;
+
+#!/bin/bash
+# cross compilation tools
 echo 'INSTALLING CROSS COMPILTATION TOOLS'
-sudo dpkg --add-architecture i386 && sudo apt update && sudo apt -y install wine32 mingw-w64;
+echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_MODE=a 
+sudo dpkg --add-architecture i386 && sudo apt -y update && sudo apt -y install wine32 mingw-w64;
 # csharp
 sudo apt -y install mono-complete;
